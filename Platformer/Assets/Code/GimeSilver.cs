4f35753783a3cbaf235f850960a4bbb1ca8f3696
Vladimira <vladimirakolar@gmail.com>
2020-02-06 23:30:13 +0100

    Set the audio to new object and when we collect the object make to destroy it.

---
+++ Platformer/Assets/Code/GimeSilver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GimeSilver : MonoBehaviour
+{
+    public GameObject scoreBox;
+    public AudioSource collectSound;
+
+    void OnTriggerEnter()
+    {
+        scoreBox.GetComponent<Text>().text = "1000";
+        collectSound.Play();
+        Destroy(gameObject);
+    }
+}
