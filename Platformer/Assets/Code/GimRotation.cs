77733f0c67aa754f648cb33ab351e68b20eb76d8
Vladimira <vladimirakolar@gmail.com>
2020-02-06 23:02:45 +0100

    Created text cavas, new object, new script for object, added point light.

---
+++ Platformer/Assets/Code/GimRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimRotation : MonoBehaviour
+{
+    public int rotateSpeed = 2;
+
+    private void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+
+}
