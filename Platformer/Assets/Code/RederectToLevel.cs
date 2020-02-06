a3812c8fdfa2dd7d7791b561e47884a1fa5a8c93
Vladimira <vladimirakolar@gmail.com>
2020-02-07 00:10:09 +0100

    Make the character can die.

---
+++ Platformer/Assets/Code/RederectToLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RederectToLevel : MonoBehaviour
+{
+    public static int rederecToLevel = 1;
+
+
+    private void Update()
+    {
+        if (rederecToLevel == 1)
+        {
+            SceneManager.LoadScene(rederecToLevel);
+        }
+    }
+}
